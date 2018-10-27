1a69b9f883ec05ade37177bca55849d7a98a490a
Jeka98 <Zhukjeka1998@yandex.by>
2018-10-28 00:04:05 +0300

    fix: fix code with eslint

--- sumOfOther/sumOfOther.js
+++ sumOfOther/sumOfOther.js
@@ -1,15 +1,15 @@
 module.exports = function sumOfOther(count = []) {
-    const result = [];
-    
-    for (let i=0, len = count.length; i<len; i++) {
-        let counter = 0;
-        
-        for (let j=0, len = count.length; j<len; j++) {
-            if (j != i) {
-                counter += count[j];
-            }
-        }
-        result.push(counter);
+  const result = [];
+
+  for (let i = 0, len = count.length; i < len; i++) {
+    let counter = 0;
+
+    for (let j = 0, len = count.length; j < len; j++) {
+      if (j !== i) {
+        counter += count[j];
+      }
     }
-    return result;
+    result.push(counter);
+  }
+  return result;
 };
